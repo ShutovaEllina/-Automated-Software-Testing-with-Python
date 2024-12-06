60fca995977c58f7cf149ff4507f9a4775d6b70a
Ellina Shutova <ellina.shutova@smiledirectclub.com>
2024-12-06 21:06:50 +0400

    13 and 14 lessons

---
+++ 14-/Booleans.py
@@ -0,0 +1,26 @@
+# print(5 == 5) # true
+# print(5 > 5) # false
+# print(10 != 10) # false
+
+#comparisons: ==, !=, <, >, <=, >=
+
+
+
+
+
+# friends = ["Rolf", "Bob"]
+# abroad = ["Rolf", "Bob"]
+
+# print(friends == abroad) # true
+# print(friends is abroad) # false, another list -> another memory, the're not the same
+# print(friends is friends) # true
+
+
+
+
+
+friends = ["Rolf", "Bob"]
+abroad = friends
+
+print(friends == abroad) # true
+print(friends is abroad) # true
